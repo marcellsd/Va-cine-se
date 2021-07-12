882aefef7b50ea02b45b00ed3a0fd1fde259df2c
marcellsd <56489714+marcellsd@users.noreply.github.com>
2021-07-12 18:52:15 -0300

    Add files via upload

---
+++ va-cine-se/src/main/java/br/com/vacine_se/VaCineSeApplication.java
@@ -0,0 +1,13 @@
+package br.com.vacine_se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaCineSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaCineSeApplication.class, args);
+	}
+
+}
---
+++ va-cine-se/src/test/java/br/com/vacine_se/VaCineSeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vacine_se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaCineSeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
