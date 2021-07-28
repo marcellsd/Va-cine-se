bb318b63c7f51f30ec2d6edcd81be28185bfb890
Marcell Santana de Deus <marcellsantana@gmail.com>
2021-07-27 22:09:39 -0300

    District init OK

--- va-cine-se/src/main/java/br/com/vacine_se/district/District.java
+++ va-cine-se/src/main/java/br/com/vacine_se/district/District.java
@@ -3,7 +3,11 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
+
 import org.springframework.data.annotation.Id;
+
+import br.com.vacine_se.utils.Coordinate;
+
 
 public class District {
 	@NotNull(message = "must be a District ID")
@@ -12,10 +16,12 @@ public class District {
 	@NotNull(message = "must be a district name")
 	@Pattern(regexp = "^[a-zA-Z ]+$", message = "name must be a string")
 	private final String name;
+	private Coordinate<Integer, Integer> coordinate;
 	
-	public District(Long id, String name){
+	public District(Long id, String name, Integer coordX, Integer coordY){
 		this.id = id;
 		this.name = name;
+		this.coordinate = new Coordinate<Integer, Integer>(coordX,coordY);
 	}
     @Id
 	public Long getId() {
@@ -24,5 +30,12 @@ public class District {
 	
 	public String getName() {
 		return this.name;
+	}
+
+	public Integer getCoordnateX() {
+		return this.coordinate.getX();
+	}
+	public Integer getCoordnateY() {
+		return this.coordinate.getY();
 	}
 }
--- va-cine-se/src/main/java/br/com/vacine_se/district/DistrictService.java
+++ va-cine-se/src/main/java/br/com/vacine_se/district/DistrictService.java
@@ -23,8 +23,42 @@ public class DistrictService {
 	// TODO: Adicionar todos os bairros
 	private static List<District> getDistrictFromData(){
 		return List.of(
-				new District(1L, "Neópolis"),
-				new District(2L, "Candelária")
+				new District(1L, "Alecrim",2 ,3),
+				new District(2L, "Areia Preta",3 ,3),
+				new District(3L, "Barro Vermelho",3 ,3),
+				new District(4L, "Bom Pastor",1 ,3),
+				new District(5L, "Candelária",2 ,1),
+				new District(6L, "Capim Macio",3 ,1),
+				new District(7L, "Cidade Alta",3 ,4),
+				new District(8L, "Cidade Esperança",2 ,2),
+				new District(9L, "Cidade Nova",1 ,2),
+				new District(10L, "Dix-sept Rosado",2 ,3),
+				new District(11L, "Felipe Camarão",1 ,2),
+				new District(12L, "Guararapes",0 ,2),
+				new District(13L, "Igapó",1 ,4),
+				new District(14L, "Lagoa Azul",1 ,6),
+				new District(15L, "Lagoa Nova", 2, 2),
+				new District(16L, "Lagoa Seca",3 ,3),
+				new District(16L, "Mãe Luíza",4 ,3),
+				new District(16L, "Neópolis",3 ,1),
+				new District(16L, "Nordeste",1 ,3),
+				new District(16L, "Nossa Senhora da Apresentação",0 ,5),
+				new District(16L, "Nossa Senhora de Nazaré",2 ,3),
+				new District(16L, "Nova Descoberta",3, 2),
+				new District(16L, "Pajuçara",2 ,6),
+				new District(16L, "Petrópolis",3 ,4),
+				new District(16L, "Pitimbu",2 ,1),
+				new District(16L, "Planalto",1 ,1),
+				new District(16L, "Ponta Negra",0 ,4),
+				new District(16L, "Potengi",1 ,5),
+				new District(16L, "Praia do Meio",3 ,4),
+				new District(16L, "Quintas",2 ,3),
+				new District(16L, "Redinha",2 ,5),
+				new District(16L, "Ribeira",3 ,4),
+				new District(16L, "Rocas",3 ,4),
+				new District(16L, "Salinas",1 ,4),
+				new District(16L, "Santos Reis",3 ,5),
+				new District(16L, "Tirol",3 ,3)
 				);
 	}
 	
